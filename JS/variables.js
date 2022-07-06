65a85fbcb46c90e9e43584e32aad0fb37cd4c18b
Hector <Hectormamani18@hotmail.com>
2022-07-06 15:43:37 -0300

    agregre librerias

--- JS/variables.js
+++ JS/variables.js
@@ -2,13 +2,17 @@ document.addEventListener("submit",()=>{
     convertir()
 })
 
+
+
 const  Moneda = []
 const  Valor  = []
 
 const inputNombre = document.querySelector("#inputNombre")
 const inputValor  = document.querySelector("#inputValor")
+const btnSubmit = document.querySelector("#submit")
 
 
+btnSubmit.addEventListener("click", ()=> datosFalse() )
 
 
 
